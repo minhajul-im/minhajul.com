e38319ecc54bcc2bb6553f994b367fdbe858678c
minhajul-im <minhajul.cpp@gmail.com>
2023-08-11 00:14:39 +0600

    icon folder updated

--- src/app/components/icon/page.jsx
+++ src/app/components/icon/page.jsx
@@ -6,13 +6,11 @@ config.autoAddCss = false;
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
-  faLaptop,
-  faCode,
-  faProcedures,
-  faAddressBook,
-  faGauge,
-} from "@fortawesome/free-solid-svg-icons";
-import {faFacebook, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+  faFacebook,
+  faLinkedinIn,
+  faTwitter,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const links = [
   {
@@ -22,22 +20,17 @@ const links = [
   },
   {
     id: 2,
-    name: faCode,
+    name: faGithub,
     link: "https://github.com/minhajul-im",
   },
   {
     id: 3,
-    name: faProcedures,
-    link: "",
-  },
-  {
-    id: 4,
     name: faFacebook,
     link: "https://www.facebook.com/minhajul.im",
   },
   {
-    id: 5,
-    name: faGauge,
+    id: 4,
+    name: faTwitter,
     link: "https://twitter.com/minhajul_im",
   },
 ];
@@ -47,11 +40,7 @@ const Icon = () => {
     <div className="flex items-baseline gap-6">
       {links.map((item) => (
         <a key={item.id} href={item.link} target="_blank">
-          <FontAwesomeIcon
-            icon={item.name}
-            // className="text-2xl text-blue-500"
-            style={{ fontSize: 35 }}
-          />
+          <FontAwesomeIcon icon={item.name} style={{ fontSize: 35 }} />
         </a>
       ))}
     </div>
